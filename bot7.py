42b64260833d5408d4d134f66dc3182eb3ac8aa9
iruzts <ruizsoto1996@gmail.com>
2021-08-01 15:50:32 -0600

    correccion de error

--- bot7.py
+++ bot7.py
@@ -11,17 +11,17 @@ get_me = bot.get_me()
 @bot.message_handler(commands=['info','hola']) #definimos el comando
 def hola(mensaje):
     bot.send_chat_action(id, 'typing')
-    bot.send_message(id, "Envia 1 o Celsius par ver Formula Grados Celsius a Grados Farenheit \n Envia 2 o Farenheit para ver Formula Grados Farenheit a Grados Celsius.")
-    print("Mandaron 1")
+    bot.send_message(id, "Envia /1 o /Celsius par ver Formula Grados Celsius a Grados Farenheit \n Envia /2 o /Farenheit para ver Formula Grados Farenheit a Grados Celsius.")
+    print("Mandaron info")
 
 @bot.message_handler(commands=['1','Celsius']) 
-def hola(mensaje):
+def Celsius(mensaje):
     bot.send_chat_action(id, 'typing')
     bot.send_message(id, "(0 °C × 9/5) + 32 = 32 °F")
     print("Mandaron 1")
 
 @bot.message_handler(commands=['2','Farenheit'])
-def nombre(mensaje):
+def Farenheit(mensaje):
     bot.send_chat_action(id, 'typing')
     bot.send_message(id, "(32 °F − 32) × 5/9 = 0 °C")
     print("Mandaron 2")
